ecd61a2670564b6b1303b1d607010facc04b99f1
Nikich <nikich.nicolos@gmail.com>
2019-01-22 18:55:58 +0300

    deploy

---
+++ precache-manifest.806cebef6a49c5be24d1b471f26ad150.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tr-logic-test/robots.txt"
+  },
+  {
+    "revision": "8a4acddbeaa235c17c29",
+    "url": "/tr-logic-test/js/pages-catalog.06e986ba.js"
+  },
+  {
+    "revision": "828289d68704022e5302",
+    "url": "/tr-logic-test/js/pages-basket.b1d1eac9.js"
+  },
+  {
+    "revision": "b3323747bac1d74abd41",
+    "url": "/tr-logic-test/js/chunk-vendors.bee3ee6e.js"
+  },
+  {
+    "revision": "0f11ae29fe69936135d0",
+    "url": "/tr-logic-test/js/app.32fe7974.js"
+  },
+  {
+    "revision": "708b787236858cec4d06f5cfdd980b25",
+    "url": "/tr-logic-test/index.html"
+  },
+  {
+    "revision": "8a4acddbeaa235c17c29",
+    "url": "/tr-logic-test/css/pages-catalog.2ba6111d.css"
+  },
+  {
+    "revision": "828289d68704022e5302",
+    "url": "/tr-logic-test/css/pages-basket.0b23b7bf.css"
+  },
+  {
+    "revision": "0f11ae29fe69936135d0",
+    "url": "/tr-logic-test/css/app.06b386ac.css"
+  }
+];
